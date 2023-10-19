690222b917363dd3878ed0f4886cf88ee5e98018
Chadeya-MCR <chadeyamorgan203@gmail.com>
2023-10-19 16:56:35 +0300

    Program that prints hello wolrd

---
+++ hello_world.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * hello_world - Prints "Hello, world!" to the console.
+ */
+void hello_world(void)
+{
+	printf("Hello, world!\n");
+}
+
+/**
+ * main - Entry point to the main program
+ *
+ * Return: 0 for success
+ */
+int main(void)
+{
+	hello_world();
+	return (0);
+}
+
